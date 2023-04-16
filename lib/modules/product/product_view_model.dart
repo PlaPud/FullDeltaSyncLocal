f614e4e23fcf8166583396916a0f2acea925ecd4
PlaPud <patcub252545@gmail.com>
2023-04-16 21:08:51 +0700

    change casting function to class factory method

--- lib/modules/product/product_view_model.dart
+++ lib/modules/product/product_view_model.dart
@@ -3,7 +3,6 @@ import 'package:full_delta_sync/modules/product/product_model.dart';
 import 'package:hive/hive.dart';
 
 class ProductViewModel {
-
   List<Product> readData() {
     var productBox = Hive.box<Product>('products');
     List<Product> products = productBox.values.toList().cast<Product>();
@@ -29,7 +28,8 @@ class ProductViewModel {
 
   deltaSyncWrite(String productListData) async {
     List<dynamic> decodedData = json.decode(productListData);
-    List<Product> productList = _parseToHiveObject(decodedData);
+    List<Product> productList =
+        decodedData.map((json) => Product.fromJson(json)).toList();
     var productBox = Hive.box<Product>('products');
     try {
       for (var product in productList) {
