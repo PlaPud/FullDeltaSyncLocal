3e7822b30cefd65a3bc5098a25d83573a3c6b0d8
Winterjrp <jaroonpong.s@ku.th>
2023-04-16 20:25:26 +0700

    UI delta sync

--- lib/modules/product/product_view.dart
+++ lib/modules/product/product_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// import 'package:full_delta_sync/button.dart';
 import 'package:full_delta_sync/modules/product/product_model.dart';
 import 'package:full_delta_sync/modules/product/product_view_model.dart';
 import 'package:hive/hive.dart';
@@ -26,31 +28,44 @@ class _ProductViewState extends State<ProductView> {
     return Column(
       children: [
         Container(
-          padding: EdgeInsets.all(30.0),
+          padding: const EdgeInsets.all(30.0),
           child: TextField(
+            decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: "Put your JSON data here..",
+                hintStyle: TextStyle(color: Colors.grey),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
             controller: textEditingController,
             onChanged: (data) {
               setState(() {
                 inputText = data;
               });
             },
-            maxLines: 1,
+            maxLines: 7,
           ),
         ),
-        Center(
-          child: ElevatedButton(
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          ElevatedButton(
             onPressed: () {
               setState(() {
                 productList = productViewModel.readData();
               });
             },
-            child: Text(
+            style: ButtonStyle(
+              foregroundColor: getColor(Colors.white, Colors.teal),
+              backgroundColor: getColor(Colors.teal, Colors.white),
+            ),
+            child: const Text(
               'Read JSON',
             ),
           ),
-        ),
-        Center(
-          child: ElevatedButton(
+          ElevatedButton(
+            style: ButtonStyle(
+              foregroundColor: getColor(Colors.white, Colors.teal),
+              backgroundColor: getColor(Colors.teal, Colors.white),
+            ),
             onPressed: () async {
               await productViewModel.fullSyncWrite(inputText);
               setState(() {
@@ -58,24 +73,60 @@ class _ProductViewState extends State<ProductView> {
                 textEditingController.clear();
               });
             },
-            child: Text(
-              'Write JSON (Full Sync)',
+            child: const Text(
+              'Write (Full)',
             ),
           ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: productList.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(
-                  '{title: "${productList[index].title}", price: ${productList[index].price}, remainInStock: ${productList[index].remainInStock}',
-                ),
-              );
+          ElevatedButton(
+            onPressed: () async {
+              await productViewModel.deltaSyncWrite(inputText);
+              setState(() {
+                productList = productViewModel.readData();
+                // textEditingController.clear();
+              });
             },
+            style: ButtonStyle(
+              foregroundColor: getColor(Colors.white, Colors.teal),
+              backgroundColor: getColor(Colors.teal, Colors.white),
+            ),
+            child: const Text(
+              'Write (Delta)',
+            ),
+          )
+        ]),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: ListView.builder(
+                itemCount: productList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      '{title: "${productList[index].title}", price: ${productList[index].price}, remainInStock: ${productList[index].remainInStock}}',
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
         ),
       ],
     );
+  }
+
+  MaterialStateProperty<Color> getColor(Color color, Color colorPressed) {
+    getColor(Set<MaterialState> states) {
+      if (states.contains(MaterialState.pressed)) {
+        return colorPressed;
+      } else {
+        return color;
+      }
+    }
+
+    return MaterialStateProperty.resolveWith(getColor);
   }
 }
--- lib/modules/product/product_view_model.dart
+++ lib/modules/product/product_view_model.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:full_delta_sync/modules/product/product_model.dart';
 import 'package:hive/hive.dart';
 
@@ -7,7 +6,6 @@ class ProductViewModel {
   List<Product> _parseToHiveObject(List<dynamic> decodedData) {
     return decodedData.map((product) {
       var hiveObject = Product();
-
       hiveObject.title = product['title'];
       hiveObject.aliasTitle = product['aliasTitle'];
       hiveObject.barcode = product['barcode'];
@@ -28,9 +26,22 @@ class ProductViewModel {
   fullSyncWrite(String productListData) async {
     List<dynamic> decodedData = json.decode(productListData);
     List<Product> productList = _parseToHiveObject(decodedData);
-
     var productBox = Hive.box<Product>('products');
     await productBox.clear();
+    try {
+      for (var product in productList) {
+        productBox.put(product.sku, product);
+      }
+      print('Write Complete!');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  deltaSyncWrite(String productListData) async {
+    List<dynamic> decodedData = json.decode(productListData);
+    List<Product> productList = _parseToHiveObject(decodedData);
+    var productBox = Hive.box<Product>('products');
     try {
       for (var product in productList) {
         productBox.put(product.sku, product);
