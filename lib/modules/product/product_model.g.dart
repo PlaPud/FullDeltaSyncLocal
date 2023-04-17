7aec1e2e6b666f19af7dfe25b6ae67fa72a19d1f
PlaPud <patcub252545@gmail.com>
2023-04-17 23:51:58 +0700

    Pushed Adapter Generator

---
+++ lib/modules/product/product_model.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProductAdapter extends TypeAdapter<Product> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Product read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Product(
+      title: fields[0] as String,
+      aliasTitle: fields[1] as String,
+      barcode: fields[2] as String,
+      sku: fields[3] as String,
+      price: fields[4] as double,
+      remainInStock: fields[5] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Product obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.aliasTitle)
+      ..writeByte(2)
+      ..write(obj.barcode)
+      ..writeByte(3)
+      ..write(obj.sku)
+      ..writeByte(4)
+      ..write(obj.price)
+      ..writeByte(5)
+      ..write(obj.remainInStock);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
